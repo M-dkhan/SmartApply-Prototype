6b7d9fc99da307f6df9852bd5d0b3b8fbdc3ae83
MD-KHAN <mdzaidkhan628@gmail.com>
2024-05-29 00:21:41 -0400

    BugFixes

--- module2/firebase_setup.py
+++ module2/firebase_setup.py
@@ -1,8 +1,15 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 class FirestoreManager:
+    # Get the parent directory path
+    parent_dir = os.path.dirname(os.getcwd())
+    
+    # Construct the path to the JSON file in the parent directory
+    json_file_path = os.path.join(parent_dir, "testproject.json")
+
     # Initialize Firebase Admin SDK and Firestore client
-    cred = credentials.Certificate("/home/zaid/SmartApply-Prototype/testproject.json")
+    cred = credentials.Certificate(json_file_path)
     firebase_admin.initialize_app(cred)
     db = firestore.client()
