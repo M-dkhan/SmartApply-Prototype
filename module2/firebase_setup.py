d186b7ea1b087c2ba6eda79659b3e2eab966acd7
MD-KHAN <mdzaidkhan628@gmail.com>
2024-05-30 15:28:46 -0400

    Fixed Path for firestore

--- module2/firebase_setup.py
+++ module2/firebase_setup.py
@@ -3,13 +3,13 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 class FirestoreManager:
-    # Get the parent directory path
-    parent_dir = os.path.dirname(os.getcwd())
+    # Get the directory path of the current script
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    print(current_dir)
     
     # Construct the path to the JSON file in the parent directory
-    json_file_path = os.path.join(parent_dir, "SmartApply-Prototype/testproject.json")
-    print(parent_dir)
-
+    json_file_path = os.path.join(current_dir, "../testproject.json")
+    print(json_file_path)
     # Initialize Firebase Admin SDK and Firestore client
     cred = credentials.Certificate(json_file_path)
     firebase_admin.initialize_app(cred)
